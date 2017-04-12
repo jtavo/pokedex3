2f15db38ea342cd167d732cfc5e2b813f7b5f061
Juan Gustavo <jgustavo@MacBook-Pro-de-Juan.local>
2017-04-12 00:06:06 -0500

    Added Pokemon Class

---
+++ pokedex3/Pokemon.swift
@@ -0,0 +1,30 @@
+//
+//  Pokemon.swift
+//  pokedex3
+//
+//  Created by Juan Gustavo on 11/04/17.
+//  Copyright © 2017 Juan Gustavo. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        
+        return _name
+    }
+    
+    var pokedexId: Int {
+        
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+}
