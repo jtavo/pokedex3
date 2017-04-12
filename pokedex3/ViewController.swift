f232d9e6007e7655f0f36d02a12b173a1f3f7f86
Juan Gustavo <jgustavo@MacBook-Pro-de-Juan.local>
2017-04-11 23:19:59 -0500

    My first commit

---
+++ pokedex3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedex3
+//
+//  Created by Juan Gustavo on 11/04/17.
+//  Copyright © 2017 Juan Gustavo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
