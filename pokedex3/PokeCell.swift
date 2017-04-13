c163b8363dbb3c0c814698e1a165d2a22379c9e9
Juan Gustavo <jgustavo@MacBook-Pro-de-Juan.local>
2017-04-12 23:54:19 -0500

    Custom collection view cell

---
+++ pokedex3/PokeCell.swift
@@ -0,0 +1,25 @@
+//
+//  PokeCell.swift
+//  pokedex3
+//
+//  Created by Juan Gustavo on 12/04/17.
+//  Copyright © 2017 Juan Gustavo. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    func configureCell(pokemon: Pokemon) {
+        
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalized
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+}
